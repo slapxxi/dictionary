2cfa7f18c01588f3b4b07bb9e337a917cc706fdb
slavapavlutin <sl.pavlutin@gmail.com>
2018-01-30 16:49:25 +0300

    Remake

--- src/App.js
+++ src/App.js
@@ -1,64 +1,18 @@
 // @flow
 import React from 'react';
-import { Provider, connect } from 'react-redux';
+import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import glamorous from 'glamorous';
-import { NOT_FOUND } from 'redux-first-router';
 import configureStore from './store/configureStore';
-import { openModal, closeModal } from './store/actions';
-import { Header, Route, Navigation } from './components/';
-import {
-  Profile,
-  Search,
-  PageNotFound,
-  Home,
-  Settings,
-} from './screens/';
 
-type Props = {
-  openModal: () => void,
-};
-
-function App({ openModal }: Props) {
-  return (
-    <Container>
-      <Navigation />
-      <Header onMenuClick={openModal} />
-      <Content>
-        <Route to="/">
-          <Home />
-        </Route>
-        <Route to="/profile">
-          <Profile />
-        </Route>
-        <Route to="/search">
-          <Search />
-        </Route>
-        <Route to="/settings">
-          <Settings />
-        </Route>
-        <Route to={NOT_FOUND}>
-          <PageNotFound />
-        </Route>
-      </Content>
-    </Container>
-  );
+function App() {
+  return <Container>Content</Container>;
 }
 
 const Container = glamorous.div({
   display: 'flex',
   flexDirection: 'column',
-  minHeight: '100vh',
 });
-
-const Content = glamorous.div({
-  display: 'flex',
-  flex: 1,
-});
-
-const enhance = connect(() => ({}), { openModal, closeModal });
-
-const AppContainer = enhance(App);
 
 function Root() {
   const { store, persistor } = configureStore();
@@ -68,7 +22,7 @@ function Root() {
         loading={<div>Loading...</div>}
         persistor={persistor}
       >
-        <AppContainer />
+        <App />
       </PersistGate>
     </Provider>
   );
