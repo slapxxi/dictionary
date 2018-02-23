407dc8f666d214da89cf9588f4eb4afce9eb1037
slavapavlutin <sl.pavlutin@gmail.com>
2018-02-23 15:03:07 +0300

    Remove redundant files

--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,6 @@ import List from './List';
 import Header from './Header';
 import Modal from './Modal';
 import Button from './Button';
-import Words from './Words';
 import WordList from './WordList';
 import Route from './Route';
 import Navigation from './Navigation';
@@ -17,7 +16,6 @@ export {
   Button,
   Route,
   Navigation,
-  Words,
   WordList,
   Keyboard,
   Slider,
